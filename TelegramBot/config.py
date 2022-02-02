fe303575d110564157af9eecd7ba171b90c23f95
Yekaterina Mikhalchuk <83668755+yekaterinamikhalchuk@users.noreply.github.com>
2022-02-02 22:04:07 +0300

    Add files via upload

---
+++ TelegramBot/config.py
@@ -0,0 +1,13 @@
+TOKEN = '5100675020:AAGrhHbMNpN1N4eqlBUkrNeOCjL8NAj2z-E'
+keys = {
+        'euro': 'EUR',
+        'ruble': 'RUB',
+        'dollar': 'USD',
+        'pound': 'GBP',
+        'yen': 'JPY',
+        'tenge': 'KZT',
+        'franc': 'CHF',
+        'yuan': 'CNY',
+        'baht': 'THB',
+        'dirham': 'AED'
+}
